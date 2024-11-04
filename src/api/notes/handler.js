67424ca0ebada19a6b705b98b6976580d012436a
Rio Hanson <riohansonmanalu@gmail.com>
2024-11-04 11:28:01 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -11,10 +11,10 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { title = 'untitled', tags, body } = request.payload;
-    const noteId = this._service.addNote({ title, tags, body });
+    const noteId = await this._service.addNote({ title, tags, body });
     const response = h.response({
       status : 'success',
       message : 'Catatan berhasil ditambahkan',
@@ -26,8 +26,8 @@ class NotesHandler {
     return response;
   }
 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status : 'success',
       data : {
@@ -36,9 +36,9 @@ class NotesHandler {
     }
   }
 
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     const { id } = request.params;
-    const note = this._service.getNoteById(id);
+    const note = await this._service.getNoteById(id);
     return {
       status : 'success',
       data : {
@@ -47,10 +47,10 @@ class NotesHandler {
     };
   }
 
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { id } = request.params;
-    this._service.editNoteById(id, request.payload);
+    await this._service.editNoteById(id, request.payload);
 
     return{
       status : 'success',
@@ -58,9 +58,9 @@ class NotesHandler {
     };
   }
 
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     const { id } = request.params;
-    this._service.deleteNoteById(id);
+    await this._service.deleteNoteById(id);
 
     return {
       status : 'success',
