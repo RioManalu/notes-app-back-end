89bbcd3cd61ba507b68792945605db9c02951f56
Rio Hanson <riohansonmanalu@gmail.com>
2024-11-12 21:27:22 +0700

    collaboration feature implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -6,8 +6,9 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class NotesService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
   }
 
   async addNote({ title, body, tags, owner }) {
@@ -30,8 +31,18 @@ class NotesService {
   }
 
   async getNotes(owner) {
+    
+    /* 
+    Data notes yang dihasilkan berpotensi duplikasi, 
+    sehingga di akhir kueri, kita GROUP nilainya agar 
+    menghilangkan duplikasi yang dilihat berdasarkan notes.id.
+    */
+
     const query = {
-      text : 'SELECT * FROM notes WHERE owner = $1',
+      text : `SELECT notes.* FROM notes 
+      LEFT JOIN collaborations ON collaborations.note_id = notes.id 
+      WHERE notes.owner = $1 OR collaborations.user_id = $1
+      GROUP BY notes.id`,
       values : [owner],
     }
     const result = await this._pool.query(query);
@@ -96,6 +107,21 @@ class NotesService {
 
     if(note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
